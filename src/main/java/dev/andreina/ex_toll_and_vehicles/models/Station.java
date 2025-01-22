56d69c99a88152320539886309a22283a6f042ca
Andreina2 <andreinaarias5@gmail.com>
2025-01-22 22:23:44 +0100

    create station constructor

--- src/main/java/dev/andreina/ex_toll_and_vehicles/models/Station.java
+++ src/main/java/dev/andreina/ex_toll_and_vehicles/models/Station.java
@@ -1,5 +1,6 @@
 package dev.andreina.ex_toll_and_vehicles.models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Station {
@@ -7,5 +8,18 @@ public class Station {
     public String city;
     public double totalCollected;
     public List<Vehicle> vehicleList;
+
+
+    // constructor
+    public Station(String name, String city) {
+        this.name = name;
+        this.city = city;
+        this.totalCollected = 0.0;
+        this.vehicleList = new ArrayList<>();
+    }
+
+
+
+
 
 }
