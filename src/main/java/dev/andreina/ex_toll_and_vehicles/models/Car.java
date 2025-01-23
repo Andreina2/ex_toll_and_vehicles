f980769567c800d47a9064e46027dbd3afe9770b
Andreina2 <andreinaarias5@gmail.com>
2025-01-23 11:09:14 +0100

    create Car class wit attribute, constructor, @override, and modified code in Station and Vehicle class

---
+++ src/main/java/dev/andreina/ex_toll_and_vehicles/models/Car.java
@@ -0,0 +1,16 @@
+package dev.andreina.ex_toll_and_vehicles.models;
+
+// extends : hereda atributos y metodos de clase vehicle
+class Car extends Vehicle {
+
+    //constructor
+    public Car (String plate) {
+        super(plate); // super: llama el constructor de la clase padre: vehicle, el constructor vehicle debe aceptar el argumento plate:placa
+    }
+
+    @Override //@Override sobreescribe el metodo de la clase vehicle
+    public double calculateToll() {
+        return 100;
+    }
+
+}
